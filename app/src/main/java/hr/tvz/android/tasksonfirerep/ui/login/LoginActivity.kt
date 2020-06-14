608da452427a5a8574509bfc89e9d7f32ce1dbd6
Valentin Rep <valentin.rep@gmail.com>
2020-06-14 19:27:53 +0200

    adding Firebase Login

--- app/src/main/java/hr/tvz/android/tasksonfirerep/ui/login/LoginActivity.kt
+++ app/src/main/java/hr/tvz/android/tasksonfirerep/ui/login/LoginActivity.kt
@@ -2,11 +2,15 @@ package hr.tvz.android.tasksonfirerep.ui.login
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GetTokenResult
 import hr.tvz.android.tasksonfirerep.R
 import hr.tvz.android.tasksonfirerep.ui.signup.SignupActivity
 import hr.tvz.android.tasksonfirerep.ui.task.TasksActivity
@@ -15,14 +19,17 @@ import hr.tvz.android.tasksonfirerep.util.Validator
 import hr.tvz.android.tasksonfirerep.util.color
 import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity(),
-    LoginView {
+class LoginActivity : AppCompatActivity(), LoginView {
     lateinit var presenter: LoginPresenter
     private val validator = Validator()
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
 
         validator
             .add(inputUsername)
@@ -79,10 +86,25 @@ class LoginActivity : AppCompatActivity(),
     }
 
     fun signin(view: View) {
-        val username = inputUsername.editText?.text.toString()
+        val email = inputUsername.editText?.text.toString()
         val password = inputPassword.editText?.text.toString()
 
-        if (validator.result()) presenter.login(username, password)
+        if (validator.result()) {
+//            presenter.login(username, password)
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+                        user?.getIdToken(true)?.addOnSuccessListener{
+                            MySharedPreferences.saveToken(it.token!!)
+                            Log.d("TOKEN", it.token!!)
+                            presenter.onSuccess()
+                        }
+                    } else {
+                        presenter.onError(401, "Authentication failed")
+                    }
+                }
+        }
     }
 
     fun signup(view: View) {

4f84927930d1446929965be7c848b769a8ae5fec
Valentin Rep <valentin.rep@gmail.com>
2020-06-15 17:29:08 +0200

    adding .zip

--- app/src/main/java/hr/tvz/android/tasksonfirerep/TasksOnFire.kt
+++ app/src/main/java/hr/tvz/android/tasksonfirerep/TasksOnFire.kt
@@ -5,14 +5,14 @@ import android.content.Context
 
 class TasksOnFire : Application() {
     init {
-       hr.tvz.android.tasksonfirerep.TasksOnFire.Companion.instance = this
+       instance = this
     }
 
     companion object {
-        private var instance: hr.tvz.android.tasksonfirerep.TasksOnFire? = null
+        private var instance: TasksOnFire? = null
 
         fun applicationContext(): Context {
-            return hr.tvz.android.tasksonfirerep.TasksOnFire.Companion.instance!!.applicationContext
+            return instance!!.applicationContext
         }
     }
 }
